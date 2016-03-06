a983236c6edaa2a726a1c59ddee92777196c77b7
Kusatchi <krichardson25@my.bcit.ca>
2016-03-06 15:40:36 -0800

    Added files via upload

---
+++ sketch_feb22a.ino
@@ -0,0 +1,16 @@
+
+#define ESP8266_GPIO4 4
+
+void setup() 
+{
+  pinMode(ESP8266_GPIO4, OUTPUT);
+}
+
+void loop() 
+{
+  digitalWrite(ESP8266_GPIO4, LOW);
+  delay(3000);
+  digitalWrite(ESP8266_GPIO4, HIGH);
+  delay(3000);
+}
+
